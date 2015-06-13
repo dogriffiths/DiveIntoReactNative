c6101864e4ea376ec1e9c5760175ef2eebfd3325
David Griffiths <dogriffiths@gmail.com>
2015-06-13 17:13:20 +0100

    Tidy of the first file.

--- Browser.js
+++ Browser.js
@@ -15,41 +15,41 @@ var {
 } = React;
 
 var Map = React.createClass({
-  componentDidMount() {
-    this.props.eventEmitter.on('setPressed', this._onUrlSet);
-  },  
-  
-  componentWillUnmount() {
-    this.props.eventEmitter.removeAllListeners('setPressed');
-  },
-  
-  getInitialState() {
-      return {
-        url: this.props.url || 'http://www.google.com',
-      };
-  },
-  
-  _onUrlSet() {
-    this.props.onUrlSet(this.state.url);
-    this.props.navigator.pop();
-  },
+    componentDidMount() {
+        this.props.eventEmitter.on('setPressed', this._onUrlSet);
+    },  
+    
+    componentWillUnmount() {
+        this.props.eventEmitter.removeAllListeners('setPressed');
+    },
+    
+    getInitialState() {
+        return {
+            url: this.props.url || 'http://www.google.com',
+        };
+    },
+    
+    _onUrlSet() {
+        this.props.onUrlSet(this.state.url);
+        this.props.navigator.pop();
+    },
 
-  _onNavigationStateChange(navigationState) {
-    this.setState({
-      url: navigationState.url,
-    });
-  },
+    _onNavigationStateChange(navigationState) {
+        this.setState({
+            url: navigationState.url,
+        });
+    },
 
-  render() {
-      return (
-        <View style={{flex: 1}}>
-          <WebView
+    render() {
+        return (
+            <View style={{flex: 1}}>
+            <WebView
             url={this.state.url}
             onNavigationStateChange={this._onNavigationStateChange}
-          />
-        </View>
-      );
-  },
+            />
+            </View>
+        );
+    },
 });
 
 module.exports = Map;
